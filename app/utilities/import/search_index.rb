cb626fbf67381494d51f42c9dde58d5863e99574
Serge Galanin <s.galanin@gmail.com>
2018-11-01 16:26:06 +0300

    rebuild index task refactored

--- app/utilities/import/search_index.rb
+++ app/utilities/import/search_index.rb
@@ -10,6 +10,24 @@ module Utilities
       PRIORITY_POST_TITLE  = 2
       PRIORITY_LOWEST      = 1
 
+
+      def self.rebuild
+        search_index = Utilities::Import::SearchIndex.new
+
+        Unit.where(destroyed_at: nil).each do |unit|
+          search_index.add_object(unit)
+        end
+        Person.where(destroyed_at: nil).each do |person|
+          search_index.add_object(person)
+        end
+        Employment.where(destroyed_at: nil).each do |employment|
+          search_index.add_object(employment)
+        end
+
+        search_index.update!
+      end
+
+
       def initialize
         @entities = {}
       end
--- lib/tasks/rebuild_index.rake
+++ lib/tasks/rebuild_index.rake
@@ -3,17 +3,5 @@ require 'dotenv/tasks'
 # run this task as: rake rebuild_index
 
 task :rebuild_index => :environment do
-  search_index = Utilities::Import::SearchIndex.new
-
-  Unit.where(destroyed_at: nil).each do |unit|
-    search_index.add_object(unit)
-  end
-  Person.where(destroyed_at: nil).each do |person|
-    search_index.add_object(person)
-  end
-  Employment.where(destroyed_at: nil).each do |employment|
-    search_index.add_object(employment)
-  end
-
-  search_index.update!
+  Utilities::Import::SearchIndex.rebuild
 end
