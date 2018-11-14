50f7d9bc0ca8a8f38d67fc188ab47f8b15bd764f
Sergey Vershinin <accountwm@gmail.com>
2018-11-14 11:08:04 +0300

    format phones

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -29,6 +29,14 @@ class Employment < ApplicationRecord
   belongs_to :unit
 
 
+  PHONES_TYPE = {
+    /\A\d{3,4}\z/       =>  'internal',
+    /\A79\d{9}\z/       =>  'mobile',
+    /\A(\d{5,7})\z/     =>  'city',
+    /\A(7[0-8]\d{9})\z/ =>  'city',
+  }
+
+
   def as_json(options = nil)
     result = super.slice(
       'post_title', 'post_category_code',
@@ -38,11 +46,38 @@ class Employment < ApplicationRecord
       'id'          => short_id,
       'person_id'   => person_short_id,
       'unit_id'     => unit_short_id,
+      'format_phones' => format_phones_with_type,
     )
-
     result.merge!('on_vacation' => true) if on_vacation
 
     result
+  end
+
+
+  def format_phones_with_type
+    new_phones = unless phones.nil?
+      phones.map do |phone|
+        phone_patterns = I18n.t("phones.patterns")
+
+        phone_pattern_hash = phone_patterns.values.find do |pattern_hash|
+        phone =~ ::Regexp.new(pattern_hash[:regexp])
+        end
+
+        new_phone = phone.gsub(::Regexp.new(phone_pattern_hash[:regexp]), phone_pattern_hash[:format])
+        phone_type_label = I18n.t("phones.type.#{get_phone_type(phone)[1]}")
+
+        [new_phone, phone_type_label]
+      end
+    end
+
+    new_phones
+  end
+
+
+  def get_phone_type(phone)
+    PHONES_TYPE.find do |phone_type_hash|
+      phone =~ phone_type_hash[0]
+    end
   end
 
 
