9664f0fb7c7f4a9797583e5db0fbebb35c53d18e
Serge Galanin <s.galanin@gmail.com>
2018-12-05 09:15:33 +0300

    whenever fixed

--- config/deploy/staging-lvs.rb
+++ config/deploy/staging-lvs.rb
@@ -15,7 +15,10 @@ set :bundle_flags, '--deployment --local'
 
 after 'yarn:install', 'staff:fix_node_sass'
 
+set :whenever_roles, :api
 set :whenever_environment, 'production'
 set :whenever_identifier, 'staff_staging'
+set :whenever_command_environment_variables, -> { fetch(:default_env).merge(rack_env: fetch(:whenever_environment)) }
+set :whenever_path, -> { release_path }
 set :whenever_load_file, -> { "#{release_path}/config/schedule_import.rb" }
 append :linked_files, 'config/schedule_import.rb'
