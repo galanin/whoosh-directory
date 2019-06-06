df2a4cae72273b55cc3816a09cca63bae2227b2a
Serge Galanin <s.galanin@gmail.com>
2019-06-06 14:29:19 +0300

    import of person photo refactored

--- app/utilities/import/onpp/person_collection.rb
+++ app/utilities/import/onpp/person_collection.rb
@@ -68,11 +68,10 @@ module Utilities
           # puts photo_path
           if File.exists?(photo_path)
             photo_modified_time = File.mtime(photo_path)
-            if person.photo_updated_at.nil? || photo_modified_time > person.photo_updated_at || ENV['STAFF_IMPORT_FORCE_PHOTO_UPDATE']
+            if person.is_photo_stale?(photo_modified_time) || ENV['STAFF_IMPORT_FORCE_PHOTO_UPDATE']
               # puts ' do import'
               File.open(photo_path) do |f|
-                person.photo = f
-                person.photo_updated_at = Time.now
+                person.photo_file = f
                 # puts '  success'
               end
             else
