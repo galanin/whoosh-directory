3918a3642fe81ddd33cdb53f49f0a5af8235d654
Serge Galanin <s.galanin@gmail.com>
2019-06-06 14:25:27 +0300

    assigning related objects optimized

--- app/models/concerns/importable_contact.rb
+++ app/models/concerns/importable_contact.rb
@@ -4,7 +4,7 @@ module ImportableContact
   included do
 
     def link_parent_node(parent_node)
-      self.parent_node          = parent_node
+      self.parent_node_id       = parent_node&.id
       self.parent_node_short_id = parent_node&.short_id
     end
 
--- app/models/concerns/importable_employment.rb
+++ app/models/concerns/importable_employment.rb
@@ -4,25 +4,25 @@ module ImportableEmployment
   included do
 
     def link_node(node)
-      self.node          = node
+      self.node_id       = node&.id
       self.node_short_id = node&.short_id
     end
 
 
     def link_department_node(node)
-      self.department          = node
+      self.department_id       = node&.id
       self.department_short_id = node&.short_id
     end
 
 
     def link_parent_node(parent_node)
-      self.parent_node          = parent_node
+      self.parent_node_id       = parent_node&.id
       self.parent_node_short_id = parent_node&.short_id
     end
 
 
     def link_person(person)
-      self.person          = person
+      self.person_id       = person&.id
       self.person_short_id = person&.short_id
     end
 
@@ -33,11 +33,11 @@ module ImportableEmployment
 
 
     # TODO refactor
-    alias_method :importable_flush_to_db, :flush_to_db
-    def flush_to_db(new_data)
-      person&.save!
-      importable_flush_to_db(new_data)
-    end
+    # alias_method :importable_flush_to_db, :flush_to_db
+    # def flush_to_db(new_data)
+      # person&.save! # TODO: save person while demo import
+      # importable_flush_to_db(new_data)
+    # end
 
   end
 end
