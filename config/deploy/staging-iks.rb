4e575a8dfaeb798b973ac7adbafec4bd3017630f
Serge Galanin <s.galanin@gmail.com>
2019-06-25 17:00:16 +0300

    less space on staging

--- config/deploy/staging-iks.rb
+++ config/deploy/staging-iks.rb
@@ -3,5 +3,6 @@ set :deploy_to, '/home/deployer/staff_staging'
 set :branch, :master
 set :rack_env, 'production'
 set :puma_env, 'production'
+set :keep_releases, 2
 
 server 'staff', user: 'deployer', roles: %w{api web}, ssh_options: { forward_agent: true }
