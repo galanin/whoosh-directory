5ff4e5fd6e5fa97b5c143f50956aed42bfd260d1
Serge Galanin <s.galanin@gmail.com>
2019-06-28 15:59:12 +0300

    optimization

--- app/models/concerns/importable_unit.rb
+++ app/models/concerns/importable_unit.rb
@@ -5,13 +5,13 @@ module ImportableUnit
 
 
     def link_node(node)
-      self.node          = node
+      self.node_id       = node&.id
       self.node_short_id = node&.short_id
     end
 
 
     def link_head(employment)
-      self.head          = employment
+      self.head_id       = employment&.id
       self.head_short_id = employment&.short_id
     end
 
