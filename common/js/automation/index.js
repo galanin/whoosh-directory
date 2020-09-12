d8bc7fe307adac65fcbc54e112a68ca13dca515f
Daniil Antoshin <antoshindanil@ya.ru>
2020-09-12 19:50:36 +0200

    fix decaffein warnings

--- common/js/automation/index.js
+++ common/js/automation/index.js
@@ -1,12 +1,7 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import search from '@automation/search';
 import url from '@automation/url';
 
-export default (function(store) {
+export default store => {
   search(store);
   return url(store);
-});
+};
--- common/js/automation/search.js
+++ common/js/automation/search.js
@@ -1,30 +1,25 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 import { setMachineQuery, setCurrentResults } from '@actions/search';
 
 let prev_query = null;
 
 const normalizeWhitespace = string => string.replace(/\s+/, ' ').trim();
 
-export default store => store.subscribe(function() {
-  const state = store.getState();
+export default store =>
+  store.subscribe(() => {
+    const state = store.getState();
 
-  const query = state.search != null ? state.search.query : undefined;
+    const query = state.search != null ? state.search.query : undefined;
 
-  if (query !== prev_query) {
-    prev_query = query;
+    if (query !== prev_query) {
+      prev_query = query;
 
-    if (query != null) {
-      const machine_query_string = normalizeWhitespace(query);
-      store.dispatch(setMachineQuery(machine_query_string));
+      if (query) {
+        const machine_query_string = normalizeWhitespace(query);
+        store.dispatch(setMachineQuery(machine_query_string));
 
-      if (machine_query_string === '') {
-        return store.dispatch(setCurrentResults({}));
+        if (machine_query_string === '') {
+          return store.dispatch(setCurrentResults({}));
+        }
       }
     }
-  }
-});
+  });
