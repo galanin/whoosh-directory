3506d73af44d598917af6fdf60fedb081200dde6
Danil Antoshin <antoshindanil>
2020-10-17 21:10:55 +0200

    remove React.createFactory

--- common/js/components/factories.js
+++ common/js/components/factories.js
@@ -1,132 +1,134 @@
 import React from 'react';
 
-// export const span = (attrs, content) => React.createElement('span', attrs, content);
+const createFactory = (component) => (attrs, ...content) => {
+  return React.createElement(component, attrs, ...content);
+};
 
-export const div = React.createFactory('div');
-export const a = React.createFactory('a');
-export const span = React.createFactory('span');
-export const img = React.createFactory('img');
-export const input = React.createFactory('input');
+export const div = createFactory('div');
+export const a = createFactory('a');
+export const span = createFactory('span');
+export const img = createFactory('img');
+export const input = createFactory('input');
 
 import SvgIcon from '@components/common/SvgIcon';
-export const svgIcon = React.createFactory(SvgIcon);
+export const svgIcon = createFactory(SvgIcon);
 
 import SomeoneWithButtons from '@components/staff/SomeoneWithButtons';
-export const someoneWithButtons = React.createFactory(SomeoneWithButtons);
+export const someoneWithButtons = createFactory(SomeoneWithButtons);
 
 import Employee from '@components/staff/Employee';
-export const employee = React.createFactory(Employee);
+export const employee = createFactory(Employee);
 
 import EmployeeWithButtons from '@components/staff/EmployeeWithButtons';
-export const employeeWithButtons = React.createFactory(EmployeeWithButtons);
+export const employeeWithButtons = createFactory(EmployeeWithButtons);
 
 import NodeEmploymentInfo from '@components/staff/NodeEmploymentInfo';
-export const nodeEmploymentInfo = React.createFactory(NodeEmploymentInfo);
+export const nodeEmploymentInfo = createFactory(NodeEmploymentInfo);
 
 import ToCall from '@components/staff/ToCall';
-export const toCall = React.createFactory(ToCall);
+export const toCall = createFactory(ToCall);
 
 import SearchResultUnit from '@components/staff/SearchResultUnit';
-export const searchResultUnit = React.createFactory(SearchResultUnit);
+export const searchResultUnit = createFactory(SearchResultUnit);
 
 import NodeUnitInfo from '@components/staff/NodeUnitInfo';
-export const nodeUnitInfo = React.createFactory(NodeUnitInfo);
+export const nodeUnitInfo = createFactory(NodeUnitInfo);
 
 import Contact from '@components/staff/Contact';
-export const contact = React.createFactory(Contact);
+export const contact = createFactory(Contact);
 
 import ConnectedContact from '@components/staff/Contact';
-export const connectedContact = React.createFactory(ConnectedContact);
+export const connectedContact = createFactory(ConnectedContact);
 
 import ToolbarButton from '@components/common/Toolbar/button';
-export const toolbarButton = React.createFactory(ToolbarButton);
+export const toolbarButton = createFactory(ToolbarButton);
 
 import MenuButton from '@components/common/Menu/Button';
-export const menuButton = React.createFactory(MenuButton);
+export const menuButton = createFactory(MenuButton);
 
 import SettingsBooleanButtons from '@components/common/Settings/BooleanButton';
-export const settingsBooleanButtons = React.createFactory(
+export const settingsBooleanButtons = createFactory(
   SettingsBooleanButtons
 );
 
 import IconedData from '@components/contact_info/IconedData';
-export const iconedData = React.createFactory(IconedData);
+export const iconedData = createFactory(IconedData);
 
 import { Element as ScrollElement } from 'react-scroll';
-export const scrollElement = React.createFactory(ScrollElement);
+export const scrollElement = createFactory(ScrollElement);
 export { scroller } from 'react-scroll';
 
 import Birthday from '@components/contact_info/Birthday';
-export const birthday = React.createFactory(Birthday);
+export const birthday = createFactory(Birthday);
 
 import Birthdays from '@components/staff/Birthdays';
-export const birthdays = React.createFactory(Birthdays);
+export const birthdays = createFactory(Birthdays);
 
 import BirthdayShortcut from '@components/staff/BirthdayShortcut';
-export const birthdayShortcut = React.createFactory(BirthdayShortcut);
+export const birthdayShortcut = createFactory(BirthdayShortcut);
 
 import BirthdayShortcutPanel from '@components/staff/BirthdayShortcutPanel';
-export const birthdayShortcutPanel = React.createFactory(BirthdayShortcutPanel);
+export const birthdayShortcutPanel = createFactory(BirthdayShortcutPanel);
 
 import CommonAvatar from '@components/staff/CommonAvatar';
-export const commonAvatar = React.createFactory(CommonAvatar);
+export const commonAvatar = createFactory(CommonAvatar);
 
 import Silhouette from '@components/contact_info/CommonSilhouette';
-export const silhouette = React.createFactory(Silhouette);
+export const silhouette = createFactory(Silhouette);
 
 import SomeoneButtons from '@components/common/SomeoneButtons';
-export const someoneButtons = React.createFactory(SomeoneButtons);
+export const someoneButtons = createFactory(SomeoneButtons);
 
 import Phones from '@components/contact_info/Phones';
-export const phones = React.createFactory(Phones);
+export const phones = createFactory(Phones);
 
 import Email from '@components/contact_info/Email';
-export const email = React.createFactory(Email);
+export const email = createFactory(Email);
 
 import OfficeLocation from '@components/contact_info/OfficeLocation';
-export const officeLocation = React.createFactory(OfficeLocation);
+export const officeLocation = createFactory(OfficeLocation);
 
 import LunchBreak from '@components/contact_info/LunchBreak';
-export const lunchBreak = React.createFactory(LunchBreak);
+export const lunchBreak = createFactory(LunchBreak);
 
 import NodeLink from '@components/staff/NodeLink';
-export const nodeLink = React.createFactory(NodeLink);
+export const nodeLink = createFactory(NodeLink);
 
 import Node from '@components/staff/Node';
-export const node = React.createFactory(Node);
+export const node = createFactory(Node);
 
 import SearchPanel from '@components/staff/SearchPanel';
-export const searchPanel = React.createFactory(SearchPanel);
+export const searchPanel = createFactory(SearchPanel);
 
 import SettingsPanel from '@components/staff/SettingsPanel';
-export const settingsPanel = React.createFactory(SettingsPanel);
+export const settingsPanel = createFactory(SettingsPanel);
 
 import ToCallPanel from '@components/staff/ToCallPanel';
-export const toCallPanel = React.createFactory(ToCallPanel);
+export const toCallPanel = createFactory(ToCallPanel);
 
 import FavoritesPanel from '@components/staff/FavoritesPanel';
-export const favoritesPanel = React.createFactory(FavoritesPanel);
+export const favoritesPanel = createFactory(FavoritesPanel);
 
 import NodeInfo from '@components/staff/NodeInfo';
-export const nodeInfo = React.createFactory(NodeInfo);
+export const nodeInfo = createFactory(NodeInfo);
 
 import OrganizationStructure from '@components/staff/OrganizationStructure';
-export const organizationStructure = React.createFactory(OrganizationStructure);
+export const organizationStructure = createFactory(OrganizationStructure);
 
 import EmployeeInfo from '@components/staff/EmployeeInfo';
-export const employeeInfo = React.createFactory(EmployeeInfo);
+export const employeeInfo = createFactory(EmployeeInfo);
 
 import ContactInfo from '@components/staff/ContactInfo';
-export const contactInfo = React.createFactory(ContactInfo);
+export const contactInfo = createFactory(ContactInfo);
 
 import EmployeeDummyInfo from '@components/staff/EmployeeDummyInfo';
-export const employeeDummyInfo = React.createFactory(EmployeeDummyInfo);
+export const employeeDummyInfo = createFactory(EmployeeDummyInfo);
 
 import SearchResults from '@components/staff/SearchResults';
-export const searchResults = React.createFactory(SearchResults);
+export const searchResults = createFactory(SearchResults);
 
 import ToCallList from '@components/staff/ToCallList';
-export const toCallList = React.createFactory(ToCallList);
+export const toCallList = createFactory(ToCallList);
 
 import FavoritesList from '@components/staff/FavoritesList';
-export const favoritesList = React.createFactory(FavoritesList);
+export const favoritesList = createFactory(FavoritesList);
